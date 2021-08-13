8e32e6d2f5067bfd165749bc72a5b0ac2ade5d0c
Samarth Agarwal <agarwalsamarth@hotmail.com>
2021-08-13 15:51:30 -0400

    Fixed flake8 errors.

--- chord_metadata_service/restapi/api_views.py
+++ chord_metadata_service/restapi/api_views.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from django.views.decorators.cache import cache_page
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
