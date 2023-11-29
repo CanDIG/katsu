bbec9ceeee3ba9ca98231b65d067c0ead0dfb3d6
Son Chau <soncqb@gmail.com>
2023-11-29 15:09:32 -0800

    Update nested_data.py

--- chord_metadata_service/mohpackets/schemas/nested_data.py
+++ chord_metadata_service/mohpackets/schemas/nested_data.py
@@ -211,7 +211,9 @@ class DonorWithClinicalDataSchema(ModelSchema):
     followups: List[NestedFollowUpSchema] = Field(..., alias="followup_set")
     biomarkers: List[NestedBiomarkerSchema] = Field(..., alias="biomarker_set")
     exposures: List[NestedExposureSchema] = Field(..., alias="exposure_set")
-    comorbidities: List[NestedComorbiditySchema] = Field(..., alias="comorbidity_set")
+    comorbidities_csdcs: List[NestedComorbiditySchema] = Field(
+        ..., alias="comorbidity_set"
+    )
 
     class Config:
         model = Donor
