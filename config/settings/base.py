c5162768715462f182dc6ea7030b62af32790606
Son Chau <soncqb@gmail.com>
2024-09-19 09:11:33 -0700

    add static folder path

--- config/settings/base.py
+++ config/settings/base.py
@@ -130,6 +130,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "chord_metadata_service/mohpackets/docs/schemas",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
