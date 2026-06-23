0aea4d2eb319f7e3fc18a9c0861356e2064623d2
Son Chau <soncqb@gmail.com>
2026-06-22 18:48:52 -0700

    Update chord_metadata_service/mohpackets/permissible_values.py
    Co-authored-by: Marion <mshadbolt@users.noreply.github.com>

--- chord_metadata_service/mohpackets/permissible_values.py
+++ chord_metadata_service/mohpackets/permissible_values.py
@@ -559,8 +559,8 @@ TREATMENT_INTENT = [
     "Screening",
     "Forensic",
     "Not available",
-    "Combined diagnostic and therapeutic intent (SNOMED: 1279505009)",
-    "Life-sustaining intent (SNOMED: 1290527003)",
+    "Combined diagnostic and therapeutic intent",
+    "Life-sustaining intent",
 ]
 
 TREATMENT_RESPONSE_METHOD = [
