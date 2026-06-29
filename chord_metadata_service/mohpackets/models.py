83b1d09d7f29cafbe2669a8cbffad6279d81c131
Son Chau <soncqb@gmail.com>
2026-06-29 16:01:47 -0700

    Update chord_metadata_service/mohpackets/models.py
    Co-authored-by: Marion <mshadbolt@users.noreply.github.com>

--- chord_metadata_service/mohpackets/models.py
+++ chord_metadata_service/mohpackets/models.py
@@ -27,7 +27,6 @@ class AutoDateTimeField(models.DateTimeField):
 class Program(models.Model):
     program_id = models.CharField(max_length=64, primary_key=True)
     metadata = models.JSONField(null=True, blank=True)
-    study_name = models.CharField(max_length=255, null=True, blank=True)
     study_description = models.TextField(null=True, blank=True)
     program_name = models.CharField(max_length=255, null=True, blank=True)
     keywords = ArrayField(
